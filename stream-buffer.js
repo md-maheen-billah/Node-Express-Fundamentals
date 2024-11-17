3e282df35d83b307c75bc90947e68eeec54ebec1
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-17 10:45:21 +0100

    Stream and buffer, create your own server

---
+++ stream-buffer.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+//creating a server using raw node.js
+
+const server = http.createServer()
+
+//listener
+server.on('request',(req,res)=>{
+    if(req.url === '/read-file' && req.method === 'GET');
+
+    //streaming file reading
+    const readableStream = fs.createReadStream(process.cwd()+'/texts/read.txt')
+
+    readableStream.on('data',(buffer)=>{
+        res.write(buffer)
+    })
+
+    readableStream.on('end',()=>{
+        res.end('Hello from world!')
+    })
+})
+
+server.listen(5000,()=>{
+    console.log(`server is lsitening to port 5000`);
+})
